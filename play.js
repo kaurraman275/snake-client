a7185976644da162dea26bae6f97b6b00149c71f
kaurraman275 <kaurraman275@gmail.com>
2020-06-04 13:28:08 -0400

    updated play.js

--- play.js
+++ play.js
@@ -1,7 +1,30 @@
 
 
-
 const { connect } = require('./client');
 console.log('Connecting ...');
 //connect();
+const handleUserInput = function(key){
 
+    if(key === '\u0003'){
+
+        return process.exit()
+    }
+
+  }
+  //handleUserInput()
+
+
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+
+
+  }
+
+  setupInput()
+
+  
