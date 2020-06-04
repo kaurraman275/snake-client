35662b89a1a3872c92b4d98847ddd1c30ffb7fe0
kaurraman275 <kaurraman275@gmail.com>
2020-06-04 11:57:33 -0400

    added client.js

--- client.js
+++ client.js
@@ -11,10 +11,20 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+
   conn.on("data",(data) => {
 
-    console.log(data);
-  })
+    console.log( "SUcces");
+
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: RPK');
+   
+  });
+
+ 
 
   return conn;
 }
